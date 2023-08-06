adeca6ac9564a506f0402cb3d82b28f9893af74b
onrdmrkrn1 <onrdmrkrn1@gmail.com>
2023-08-06 21:57:24 +0300

    api gateway configuration added

---
+++ api-gateway/src/main/java/com/productmarketing/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.productmarketing.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import javax.swing.*;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
